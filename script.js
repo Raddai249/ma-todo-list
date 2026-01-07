bafa3e02361cc8f34a49bfab8a27a40ad2cb71f1
Raddai249 <raddaikabongo7@gmail.com>
2026-01-07 07:25:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
 function addTask() {
-  alert("Le bouton Ajouter fonctionne !");
-}
-
-function addTask() {
   const input = document.getElementById("taskInput");
   const taskText = input.value.trim();
 
@@ -14,41 +10,10 @@ function addTask() {
   const li = document.createElement("li");
   li.textContent = taskText;
 
-  // Marquer comme terminée
   li.addEventListener("click", () => {
     li.classList.toggle("done");
-    saveTasks();
-    updateCounter();
   });
 
-  // Bouton supprimer
-  const deleteBtn = document.createElement("button");
-  deleteBtn.textContent = "❌";
-  deleteBtn.onclick = () => {
-    li.remove();
-    saveTasks();
-    updateCounter();
-  };
-
-  li.appendChild(deleteBtn);
   document.getElementById("taskList").appendChild(li);
-
   input.value = "";
-  saveTasks();
-  updateCounter();
-}
-function filterTasks(type) {
-  const tasks = document.querySelectorAll("#taskList li");
-
-  tasks.forEach(task => {
-    task.style.display = ""; // reset
-
-    if (type === "active" && task.classList.contains("done")) {
-      task.style.display = "none";
-    }
-
-    if (type === "done" && !task.classList.contains("done")) {
-      task.style.display = "none";
-    }
-  });
 }
